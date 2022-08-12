2a2c0be95a62d69d7d9af7f50ffcec8fa9266e6c
jeank <jeankarlos.santiago343@gmail.com>
2022-08-12 13:50:38 -0300

    Initial commit

---
+++ ExSt001.py
@@ -0,0 +1,25 @@
+# Algoritimo 001: Primitivos identificadores
+# Jean Santiago
+# Data: 12/08/2022
+
+
+a = 1
+A = 2
+
+print(a,A)
+print(a+A)
+print("----------------------")
+
+a = 1.5
+A = 2.6
+
+print(a,A)
+print(a+A)
+print("----------------------")
+
+A = "String"
+a = "Outra string"
+
+print(a,A)
+print(a+A)
+print("----------------------")
